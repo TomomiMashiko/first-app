47fdde8385c64edc6cee478f69e5b935ec69e850
Tomomi Mashiko <tomomimashiko2nd@gmail.com>
2019-10-06 20:56:07 +0900

    add: user model

--- firstapp/users/models.py
+++ firstapp/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 # Create your models here.
+
+# todo: create this class later
+# class BaseTimestampModel(object):
+#     pass
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    pass
